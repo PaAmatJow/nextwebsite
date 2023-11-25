5255160acad238dcd6895442921f75eaed846259
Pa Amat Jow <paamatjow@gmail.com>
2023-11-25 17:17:22 +0000

    Delete unnecessary files

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,24 @@ import HeroImg from 'public/hero.png';
 export default function Home() {
 	return (
 		<div className='flex items-center gap-[100px]'>
-			<div className='flex-1 flex flex-col gap-[50px]'>
-				<h1 className='text-[72px] font-extrabold leading-none bg-gradient-to-b from-[#194c33] to-[#bbb] bg-clip-text text-transparent'>
+			<div className='flex flex-1 flex-col gap-[50px]'>
+				<h1 className='bg-gradient-to-b from-[#194c33] to-[#bbb] bg-clip-text text-[72px] font-extrabold leading-none text-transparent'>
 					Better design for your digital products.
 				</h1>
 				<p className='text-[24px] font-[300]'>
 					Turning your Idea into Reality. We bring together the teams from the
 					global tech industry.
 				</p>
-				<button className='p-[20px] bg-[#53c28b] rounded-md text-white w-fit'>See Our Works</button>
+				<button className='w-fit rounded-md bg-[#53c28b] p-[20px] text-white'>
+					See Our Works
+				</button>
 			</div>
 
 			<div className=''>
 				<Image
 					src={HeroImg}
 					alt='hero'
-					className='w-full h-[500px] object-cover'
+					className='h-[500px] w-full object-cover'
 				/>
 			</div>
 		</div>
