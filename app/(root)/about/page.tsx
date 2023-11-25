07301a1774e9ada9cc7cfd2216967a4aa5eee93c
Pa Amat Jow <paamatjow@gmail.com>
2023-11-25 19:41:53 +0000

    Fix import formatting in about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,8 +1,8 @@
-import Button from "@/app/components/shared/Button";
-import Image from "next/image"
+import Button from '@/app/components/shared/Button';
+import Image from 'next/image';
 
 const About = () => {
-  return (
+	return (
 		<div className=''>
 			<div className='relative h-[300px] w-full'>
 				<Image
@@ -11,7 +11,7 @@ const About = () => {
 					alt='team photo'
 					className='object-cover grayscale'
 				/>
-				<div className='text-white absolute bottom-[20px] left-[20px] bg-[#53c28b] p-[5px]'>
+				<div className='absolute bottom-[20px] left-[20px] bg-[#53c28b] p-[5px] text-white'>
 					<h1 className='text-[24px] font-bold'>Digital Storytellers</h1>
 					<h2 className='text-[20px] font-bold'>
 						Handcrafting award winning digital experiences
@@ -20,9 +20,9 @@ const About = () => {
 			</div>
 
 			<div className='mt-[50px] flex gap-[100px]'>
-				<div className='flex-1 flex flex-col gap-[30px]'>
+				<div className='flex flex-1 flex-col gap-[30px]'>
 					<h1 className='text-[32px] font-bold'>Who Are We?</h1>
-					<p className='text-[18px] font-[300] text-justify'>
+					<p className='text-justify text-[18px] font-[300]'>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
 						quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
 						suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
@@ -37,9 +37,9 @@ const About = () => {
 						officiis voluptatum quo ea eveniet?
 					</p>
 				</div>
-				<div className='flex-1 flex flex-col gap-[30px]'>
+				<div className='flex flex-1 flex-col gap-[30px]'>
 					<h1 className='text-[32px] font-bold'>What We Do?</h1>
-					<p className='text-[18px] font-[300] text-justify'>
+					<p className='text-justify text-[18px] font-[300]'>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
 						quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
 						suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
@@ -57,5 +57,5 @@ const About = () => {
 			</div>
 		</div>
 	);
-}
-export default About
+};
+export default About;
