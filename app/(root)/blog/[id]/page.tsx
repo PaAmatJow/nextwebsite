3f1d820cf8e60611139816bfb8c8460abfad71c1
Pa Amat Jow <paamatjow@gmail.com>
2023-11-26 17:23:13 +0000

    Refactor blog page and add images

--- app/(root)/blog/[id]/page.tsx
+++ app/(root)/blog/[id]/page.tsx
@@ -1,6 +1,70 @@
+import Image from "next/image"
+
 const BlogPosts = () => {
   return (
-    <div>BlogPosts</div>
-  )
+		<div>
+			<div className='flex gap-[50px] mb-[50px]'>
+				<div className='flex-1 flex flex-col justify-between'>
+					<h1 className='text-[32px] font-bold'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+						similique?
+					</h1>
+					<p className='text-[18px]'>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
+						fugit voluptates distinctio adipisci asperiores architecto
+						dignissimos sequi corporis dolores ullam? Debitis eligendi suscipit
+						officia accusantium ab aspernatur in porro accusamus?
+					</p>
+					<div className='flex items-center gap-[10px]'>
+						<Image
+							src='https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg'
+							alt=''
+							height={40}
+							width={40}
+							className='rounded-full object-cover'
+						/>
+						<span className=''>John Doe</span>
+					</div>
+				</div>
+
+				<div className='flex-1 relative h-[300px]'>
+					<Image
+						src='https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg'
+						alt=''
+						className='object-cover'
+						fill={true}
+					/>
+				</div>
+			</div>
+
+			<div className=''>
+				<p className='text-justify text-[#999] text-[20px]'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorum
+					perferendis quam eligendi, quibusdam minima provident debitis numquam
+					sed qui eum, tempore eius velit ea aperiam quis totam non nulla. Odio,
+					maiores repudiandae cupiditate magnam obcaecati repellat amet hic eos
+					minus dolore fugit, ipsa itaque quibusdam totam! Illo blanditiis ipsum
+					exercitationem quas. Dolorum unde nostrum distinctio ea tempora,
+					voluptas non eos rem omnis, in cum optio modi quidem voluptatibus
+					sequi quam. Perferendis, asperiores consequatur. <br /> Dignissimos in
+					non, ipsam quam corporis consequuntur. Ex ea quos quis blanditiis
+					assumenda? Voluptates, distinctio! Sapiente laudantium beatae hic
+					voluptatem accusantium perferendis animi ad eum nesciunt! Aspernatur
+					amet, dignissimos officia accusamus doloremque modi facilis pariatur
+					adipisci. Quas enim molestias id rerum ut, nisi sequi odio fugiat
+					accusantium itaque corporis incidunt quisquam, ex quod dolore!
+					Recusandae illo nisi facilis. Dolores nostrum aperiam modi, et tempore
+					porro rerum a cupiditate voluptatem saepe dolorem dolorum corrupti!
+					Reprehenderit fuga aspernatur ipsum. <br /> Atque dicta quidem
+					distinctio incidunt pariatur libero, ad quas cum porro nostrum ducimus
+					corrupti deserunt nihil perspiciatis commodi possimus, minus velit
+					saepe nemo. Repudiandae, qui. Corporis, modi ipsum quaerat id repellat
+					maiores consequatur molestias exercitationem dolorum atque rerum
+					minima dolor ipsam dolorem consequuntur. Aliquid alias doloremque
+					quidem velit eveniet.
+				</p>
+			</div>
+		</div>
+	);
 }
 export default BlogPosts
--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,6 +1,58 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
+		<div className='flex flex-col gap-[50px]'>
+			<Link href='/blog/tesdId' className='flex gap-[50px]'>
+				<div className=''>
+					<Image
+						src='https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+
+				<div className='flex flex-col gap-[10px]'>
+					<h1 className='text-[32px] font-bold'>Test</h1>
+					<p className='text-[18px] text-[#999]'>Desc</p>
+				</div>
+			</Link>
+			<Link href='/blog/testID' className='flex gap-[50px]'>
+				<div className=''>
+					<Image
+						src='https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+
+				<div className='flex flex-col gap-[10px]'>
+					<h1 className='text-[32px] font-bold'>Test</h1>
+					<p className='text-[18px] text-[#999]'>Desc</p>
+				</div>
+			</Link>
+			<Link href='/blog/tesdId' className='flex gap-[50px]'>
+				<div className=''>
+					<Image
+						src='https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+
+				<div className='flex flex-col gap-[10px]'>
+					<h1 className='text-[32px] font-bold'>Test</h1>
+					<p className='text-[18px] text-[#999]'>Desc</p>
+				</div>
+			</Link>
+		</div>
+	);
 }
 export default Blog
